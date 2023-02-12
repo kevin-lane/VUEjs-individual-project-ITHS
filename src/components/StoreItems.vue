c54af5a8d63ec196118435d90e3a11a096734357
Kevin <kevinlane3@hotmail.com>
2023-02-12 01:08:42 +0100

    Previous API seems to work now

--- src/components/StoreItems.vue
+++ src/components/StoreItems.vue
@@ -1,8 +1,7 @@
 <script setup>
   import axios from 'axios'
   import ProductCard from '../components/ProductCard.vue'
-  import store from '../store';
-</script>
+  </script>
 <script>
   export default {
     data(){
@@ -15,39 +14,18 @@
     computed: {
       //Filters between different categories
       filteredCategories(){
-        return this.products.filter(i => this.checkedCategories.includes(i.category.name))
+        return this.products.filter(i => this.checkedCategories.includes(i.category))
       },
 
     },
     methods:{
       fetchProducts(){
-            // axios.get('https://fakestoreapi.com/products')
-            // .then((response) => {
-            //   console.log(response.data);
-            //   this.products = response.data;
-            //   console.log(this.products);
-            // });
-            axios.get('https://api.escuelajs.co/api/v1/products')
-            .then((response) => {
-              console.log(response.data);
-              this.products = response.data;
-              console.log(this.products);
-            });
-            axios.get('https://api.escuelajs.co/api/v1/categories')
-            .then((response) => {
-              console.log(response.data);
-            });
-      },
-      // navigateToProduct(id, title, description, price, image){
-      //   console.log("Product clicked " + id);
-      //   console.log("Title: " + title);
-
-      //   console.log(store.state.title);
-      //   store.state.title = title;
-      //   console.log("After state change: " + store.state.title);
-
-      //   this.$router.push('/products/' + id)
-      // }
+        axios.get('https://fakestoreapi.com/products')
+        .then((response) => {
+          console.log(response.data);
+          this.products = response.data;
+        });
+      }
     },
     created() {
       this.fetchProducts();
@@ -64,103 +42,67 @@
     <fieldset>
     <legend>Category</legend>
     <span>
-      <input v-model="checkedCategories" type="checkbox" id="clothes" value="Clothes" ref="clothesCheck">
+      <input v-model="checkedCategories" type="checkbox" id="clothes" value="men's clothing" ref="clothesCheck">
       <label for="clothes">Clothes</label>
     </span>
     <span>
-      <input v-model="checkedCategories" type="checkbox" id="electronics" value="Electronics" ref="electronicsCheck">
+      <input v-model="checkedCategories" type="checkbox" id="electronics" value="electronics" ref="electronicsCheck">
       <label for="electronics">Electronics</label>
     </span>
     <span>
-      <input v-model="checkedCategories" type="checkbox" id="furniture" value="Furniture" ref="furnitureCheck">
-      <label for="furniture">Furniture</label>
-    </span>
-    <span>
-      <input v-model="checkedCategories" type="checkbox" id="shoes" value="Shoes" ref="shoesCheck">
-      <label for="shoes">Shoes</label>
-    </span>
-    <span>
-      <input v-model="checkedCategories" type="checkbox" id="others" value="Others" ref="othersCheck">
-      <label for="others">Others</label>
+      <input v-model="checkedCategories" type="checkbox" id="jewelery" value="jewelery" ref="jeweleryCheck">
+      <label for="jewelery">Jewelery</label>
     </span>
     <p>Chosen category is: {{ checkedCategories }}</p>
   </fieldset>
-    <article  class="all-products">
+  <article  class="all-products">
       <!-- v-bind, v-for and v-if is used here -->
       <section v-if="products === null">
         <p>LOADING....</p>
       </section>
-      <!-- Electonics -->
       <ProductCard
         v-else-if="this.$refs.electronicsCheck.checked"
         v-for="product in filteredCategories"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
+        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'price':product.price, 'image':product.image})"
         :id="product.id"
         :name="product.title"
         :desc="product.description"
         :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
+        :category="product.category"
+        :image="product.image"
       />
-      <!-- Furniture -->
       <ProductCard
-        v-else-if="this.$refs.furnitureCheck.checked"
+        v-else-if="this.$refs.jeweleryCheck.checked"
         v-for="product in filteredCategories"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
+        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'price':product.price, 'image':product.image})"
         :id="product.id"
         :name="product.title"
         :desc="product.description"
         :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
+        :category="product.category"
+        :image="product.image"
       />
-      <!-- Clothes -->
       <ProductCard
         v-else-if="this.$refs.clothesCheck.checked"
         v-for="product in filteredCategories"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
+        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'price':product.price, 'image':product.image})"
         :id="product.id"
         :name="product.title"
         :desc="product.description"
         :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
+        :category="product.category"
+        :image="product.image"
       />
-      <!-- Shoes -->
-      <ProductCard
-        v-else-if="this.$refs.shoesCheck.checked"
-        v-for="product in filteredCategories"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
-        :id="product.id"
-        :name="product.title"
-        :desc="product.description"
-        :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
-      />
-      <!-- Others -->
-      <ProductCard
-        v-else-if="this.$refs.othersCheck.checked"
-        v-for="product in filteredCategories"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
-        :id="product.id"
-        :name="product.title"
-        :desc="product.description"
-        :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
-      />
-      <!-- All products -->
       <ProductCard
         v-else
         v-for="product in products"
-        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'description':product.description, 'price':product.price, 'image':product.images})"
+        @click="$store.commit('navigateToProduct', {'id':product.id, 'title':product.title, 'price':product.price, 'image':product.image})"
         :id="product.id"
         :name="product.title"
         :desc="product.description"
         :price="product.price"
-        :category="product.category.name"
-        :image="product.images"
+        :category="product.category"
+        :image="product.image"
       />
     </article >
 </template>
